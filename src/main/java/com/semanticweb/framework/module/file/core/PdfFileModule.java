4042faa4f71ec9681a299fdbb4133eedb060651b
unknown <thiago@thiago-PC.(none)>
2012-04-02 23:37:02 -0300

    pdf module

--- src/main/java/com/semanticweb/framework/module/file/core/PdfFileModule.java
+++ src/main/java/com/semanticweb/framework/module/file/core/PdfFileModule.java
@@ -3,6 +3,7 @@ package com.semanticweb.framework.module.file.core;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.UUID;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -17,6 +18,14 @@ import com.semanticweb.framework.module.file.IFileModule;
  * 
  */
 public class PdfFileModule implements IFileModule {
+    private static final String PDF = ".pdf";
+    // pasta onde os arquivos pdfs serao gravados
+    private String pdfFolder;
+
+    public void setPdfFolder(String pdfFolder) {
+        this.pdfFolder = pdfFolder;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -24,25 +33,23 @@ public class PdfFileModule implements IFileModule {
     public File split(String filePath, int startPage, int endPage) {
         try {
             PdfReader reader = new PdfReader(filePath);
-            String outFile = filePath.substring(0, filePath.indexOf(".pdf")) + "-" + ".pdf";
-            
+            String outFile = generatePdfName();
             Document pdf = new Document(reader.getPageSizeWithRotation(1));
-            
             PdfCopy pdfCopy = new PdfCopy(pdf, new FileOutputStream(outFile));
             pdf.open();
-            for (int j = startPage; j <= endPage; j++) {
-                pdfCopy.addPage(pdfCopy.getImportedPage(reader, j));
-            }
-            
+            addPagesToPdfCopy(startPage, endPage, pdfCopy, reader);
             pdf.close();
             pdfCopy.close();
             return new File(outFile);
         } catch (BadPdfFormatException e) {
-            e.printStackTrace();
+            // TODO colocar log
+            System.out.println("error: " + e.getMessage());
         } catch (DocumentException e) {
-            e.printStackTrace();
+            // TODO colocar log
+            System.out.println("error: " + e.getMessage());
         } catch (IOException e) {
-            e.printStackTrace();
+            // TODO colocar log
+            System.out.println("error: " + e.getMessage());
         }
         return null;
     }
@@ -51,7 +58,57 @@ public class PdfFileModule implements IFileModule {
      * {@inheritDoc}
      */
     @Override
-    public File merge(String filePath1, String filepath2) {
+    public File merge(String filePath1, String filePath2) {
+        try {
+            PdfReader reader1 = new PdfReader(filePath1);
+            PdfReader reader2 = new PdfReader(filePath2);
+            String outFile = generatePdfName();
+            Document pdf = new Document(reader1.getPageSizeWithRotation(1));
+            PdfCopy pdfCopy = new PdfCopy(pdf, new FileOutputStream(outFile));
+            pdf.open();
+            addPagesToPdfCopy(1, reader1.getNumberOfPages(), pdfCopy, reader1);
+            addPagesToPdfCopy(1, reader2.getNumberOfPages(), pdfCopy, reader2);
+            pdf.close();
+            pdfCopy.close();
+            return new File(outFile);
+        } catch (BadPdfFormatException e) {
+            // TODO colocar log
+            System.out.println("error: " + e.getMessage());
+        } catch (DocumentException e) {
+            // TODO colocar log
+            System.out.println("error: " + e.getMessage());
+        } catch (IOException e) {
+            // TODO colocar log
+            System.out.println("error: " + e.getMessage());
+        }
         return null;
+    }
+    
+    /**
+     * 
+     * @return Retorna novo nome do pdf
+     */
+    private String generatePdfName() {
+        String name = pdfFolder + UUID.randomUUID().toString() + PDF; 
+        File file = new File(name);
+        while(file.exists()) {
+            name = pdfFolder + UUID.randomUUID().toString();
+        }              
+        return name;
+    }
+
+    /**
+     * Metodo auxiliar para adicionar paginas a um pdf
+     * @param startPage pagina inicial
+     * @param endPage pagina final
+     * @param pdfCopy novo pdf gerado
+     * @param reader leitor de um pdf
+     * @throws BadPdfFormatException 
+     * @throws IOException
+     */
+    private void addPagesToPdfCopy(int startPage, int endPage, PdfCopy pdfCopy, PdfReader reader) throws BadPdfFormatException, IOException {
+        for (int j = startPage; j <= endPage; j++) {
+            pdfCopy.addPage(pdfCopy.getImportedPage(reader, j));
+        }
     }
 }
--- src/test/java/com/semanticweb/framework/module/file/core/PdfFileModuleTest.java
+++ src/test/java/com/semanticweb/framework/module/file/core/PdfFileModuleTest.java
@@ -1,7 +1,10 @@
 package com.semanticweb.framework.module.file.core;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,15 +13,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.itextpdf.text.pdf.PdfReader;
-import com.semanticweb.framework.module.file.IFileModule;
 
 public class PdfFileModuleTest {
-    
-    private IFileModule pdfFileModule;
-    
+    private PdfFileModule pdfFileModule;
+
     @Before
     public void doBefore() {
         pdfFileModule = new PdfFileModule();
+        String path = getClass().getResource("/files/").getPath().toString();    
+        path = path.replace("file:/", "");
+        path = path.replace("/", File.separator);
+        pdfFileModule.setPdfFolder(path);
     }
 
     @Test
@@ -27,10 +32,8 @@ public class PdfFileModuleTest {
         path = path.replace("file:/", "");
         path = path.replace("/", File.separator);
         File file = pdfFileModule.split(path, 3, 10);
-        
         assertNotNull(file);
         assertTrue(file.exists());
-        
         PdfReader reader;
         try {
             reader = new PdfReader(file.getAbsolutePath());
@@ -38,10 +41,30 @@ public class PdfFileModuleTest {
         } catch (IOException e) {
             fail(e.getMessage());
         }
-        
         assertNull(pdfFileModule.split("", 1, 2));
         assertNull(pdfFileModule.split("/files/a.txt", 1, 2));
-        
-        
+    }
+
+    @Test
+    public void testMerge() {
+        String path = getClass().getResource("/files/cibercultura.pdf").getPath().toString();
+        path = path.replace("file:/", "");
+        path = path.replace("/", File.separator);
+        String path2 = getClass().getResource("/files/OntoWS.pdf").getPath().toString();
+        path2 = path2.replace("file:/", "");
+        path2 = path2.replace("/", File.separator);
+        File file = pdfFileModule.merge(path, path2);
+        assertNotNull(file);
+        assertTrue(file.exists());
+        PdfReader reader;
+        try {
+            reader = new PdfReader(file.getAbsolutePath());
+            assertEquals(70, reader.getNumberOfPages());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+        assertNull(pdfFileModule.merge("", ""));
+        assertNull(pdfFileModule.merge("/files/a.txt", "/files/OntoWS.pdf"));
+        assertNull(pdfFileModule.merge("/files/OntoWS.pdf", "/files/a.txt"));
     }
 }
